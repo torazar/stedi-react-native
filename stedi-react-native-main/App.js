f3de1d85b3304510c8f3d99b8e2573c320213029
torazar <zwe21001@byui.edu>
2022-06-09 13:32:34 -0600

    Fixin, stuffs

--- stedi-react-native-main/App.js
+++ stedi-react-native-main/App.js
@@ -8,51 +8,69 @@ import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Login from './Login.js';
+import { useState} from "react";
 
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName='Home'
-        activeColor='white'
-        barStyle={{ backgroundColor: 'green' }}
-      >
-        <Tab.Screen
-          name='Home'
-          component={Home}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='home' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Step Counter'
-          component={Counter}
-          options={{
-            tabBarLabel: 'Step Counter',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='watch' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Settings'
-          component={SettingsScreen}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name='gear' color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+
+  
+  if (userLoggedIn) {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName='Home'
+          activeColor='white'
+          barStyle={{ backgroundColor: 'green' }}
+        >
+          <Tab.Screen
+            name='Home'
+            component={Home}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='home' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Step Counter'
+            component={Counter}
+            options={{
+              tabBarLabel: 'Step Counter',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='watch' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Settings'
+            component={SettingsScreen}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name='gear' color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+
+
+  }
+  else {
+    return (
+      <View>
+        <Login />
+      </View>
+    )
+}
+
 
 
 }
---
+++ stedi-react-native-main/Login.js
@@ -0,0 +1,37 @@
+import { useState} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder="434-544-8799"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeNumber={setOneTimePassword}
+        value={oneTimePassword}
+        placeholder=""
+        keyboardType="numeric"
+        secureTextEntry={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default Login;
