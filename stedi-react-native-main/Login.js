677a09629554555bb7a58807d343385ea466b3ab
torazar <zwe21001@byui.edu>
2022-06-17 09:50:28 -0600

    login improve

--- stedi-react-native-main/Login.js
+++ stedi-react-native-main/Login.js
@@ -1,12 +1,27 @@
 import { useState} from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
+import { Text, TouchableOpacity, View } from "react-native";
+
+
+const sendText = async (phoneNumber) => {
+  await fetch('https://dev.stedi.me/twofactorlogin/' + phoneNumber, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/text'
+    }
+  })
+  console.log("PhoneNumber: ", phoneNumber);
+};
+
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.margin}>
       <TextInput
         style={styles.input}
         onChangeText={setPhoneNumber}
@@ -17,10 +32,21 @@ const Login = () => {
         style={styles.input}
         onChangeNumber={setOneTimePassword}
         value={oneTimePassword}
-        placeholder=""
+        placeholder="1234"
         keyboardType="numeric"
         secureTextEntry={true}
       />
+        <View style={buttonstyles.container}>
+      <View style={buttonstyles.countContainer}>
+        <Text>Count: {count}</Text>
+      </View>
+      <TouchableOpacity
+        style={buttonstyles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text style = {styles.text}>Press Here</Text>
+      </TouchableOpacity>
+    </View>
     </SafeAreaView>
   );
 };
@@ -32,6 +58,34 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  margin: {
+    marginTop:100
+  }
+});
+const buttonstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 100
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#000000",
+    padding: 100
+    
+    
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 100
+  },
+  text:{
+    backgroundColor: "#FFFFFF",
+    padding: 15,
+    textColor: "#FF00FF",
+    
+  }
+
 });
 
 export default Login;
